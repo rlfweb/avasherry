31e1c2b768eba17c229f5a5b62674df420babd46
Rebecca French <rlfweb8@gmail.com>
2021-01-06 13:31:08 +0000

    add Barba's beforeEnter hook

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,13 @@ barba.init({
   transitions: [
     {
       name: "fade",
+      // add Barba's beforeEnter hook to get any page to scroll smoothly to the top before it loads
+      beforeEnter() {
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+      },
     },
   ],
 });
